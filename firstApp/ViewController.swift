5fc3bef54e973ac6524eea4e7a6586704e46503c
Yunpeng Gao <yg6c2@mst.edu>
2016-12-23 16:50:18 -0600

    add btn

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
 
+    @IBOutlet weak var statePickerBtn: UIButton!
+    @IBOutlet weak var statePicker: UIPickerView!
+    let state = ["Missouri", "Alaska", "California", "Utah", "Maine", "Washington", "Oregon"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        // Do any additional setup after
         print("hi world ggg")
     }
 
@@ -20,7 +25,30 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return state.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return state[row]
+    }
+    @IBAction func stateBtnPressed(_ sender: Any) {
+        statePicker.isHidden = false
+    }
 
-
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(state[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+    @IBAction func buyBtnPressed(_ sender: Any) {
+        for view in self.view.subviews as [UIView]{
+            view.isHidden = true
+        }
+    }
+    
 }
+
 

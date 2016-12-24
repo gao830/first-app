c2d35128eb1505d1ecfec51e448dc916014c051f
Yunpeng Gao <yg6c2@mst.edu>
2016-12-24 01:51:29 -0600

    add print

--- firstApp/ViewController.swift
+++ firstApp/ViewController.swift
@@ -54,6 +54,7 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
         for view in self.view.subviews as [UIView]{
             view.isHidden = true
         }
+        print("Hi")
     }
     
 }
